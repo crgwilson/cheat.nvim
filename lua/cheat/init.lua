c3969642fcde8617c1d11574ecccfcbef3135794
Craig Wilson <craigjwilson93@gmail.com>
2020-12-06 17:41:40 -0500

    Clean everything up now that I sort of have a handle on what I'm doing

--- lua/cheat/init.lua
+++ lua/cheat/init.lua
@@ -1,84 +1,11 @@
+local query = require("cheat.query")
+
+local cheat = {}
+
 local currentFileType = vim.api.nvim_buf_get_option(0, "filetype")
-local guiStats = vim.api.nvim_list_uis()[1]
-local queryBuffer = vim.api.nvim_create_buf(false, true)
 
-local function centerInWindow(str, windowWidth)
-  local shift = math.floor(windowWidth / 2) - math.floor(string.len(str) / 2)
-  return string.rep(" ", shift) .. str
+function cheat.cheat()
+  local queryWindow = query.newQuery(currentFileType)
 end
 
-local function calculatePadding(guiSize, padAmount)
-  --[[
-    guiSize: The actual full size of the current UI. This is the value which will have the
-             padValue applied to it as described below.
-
-    padAmount: Represents the percentage value which will be used to size the border around
-               the floating window. For example, a padValue of 20 will will result in 20% of
-               the full window size.
-
-    Note: Neovim's `nvim_open_win` function expects an int not a float, so the value returned
-          by this function will be floored.
-  ]]
-  local paddingValue = math.floor(guiSize * (padAmount / 100))
-  return paddingValue
-end
-
-local function setMappings()
-  local mappings = {
-    ["<cr>"] = "runQuery()"
-  }
-
-  for k,v in pairs(mappings) do
-    vim.api.nvim_buf_set_keymap(queryBuffer, "n", k, ":lua require'cheat'." .. v .. "<cr>", {
-      nowait = true,
-      noremap = true,
-      silent = true
-    })
-  end
-end
-
-local function createQueryWindow()
-  -- TODO: I am 100% certain this whole padding system is all jacked up and needs fixing,
-  --       but it works well enough for now.
-  local widthPadding = calculatePadding(guiStats.width, 30)
-  local heightPadding = calculatePadding(guiStats.height, 30)
-
-  local windowWidth = math.floor(guiStats.width - (widthPadding * 2))
-  local windowHeight = math.floor(guiStats.height - (heightPadding * 2))
-
-  local windowHeader = centerInWindow("Need help with your " .. currentFileType .. "?", windowWidth)
-  vim.api.nvim_buf_set_option(queryBuffer, "bufhidden", "wipe")
-  vim.api.nvim_buf_set_lines(queryBuffer, 0, -1, false, {windowHeader, "", "Enter your query and press enter:", "", ""})
-
-  local queryWindow = vim.api.nvim_open_win(queryBuffer, true, {
-    relative = "editor",
-    width = windowWidth,
-    height = windowHeight,
-    col = widthPadding,
-    row = heightPadding,
-    style="minimal"
-  })
-
-  vim.api.nvim_win_set_cursor(queryWindow, {5, 0})
-
-  setMappings()
-
-  return queryWindow
-end
-
-local function runQuery()
-  print(vim.inspect(vim.api.nvim_buf_get_lines(buf, 5, -1, false)))
-end
-
-local function cheat()
-  local queryWindow = createQueryWindow()
-end
-
-return {
-  centerInWindow = centerInWindow,
-  calculatePadding = calculatePadding,
-  setMappings = setMappings,
-  createQueryWindow = createQueryWindow,
-  runQuery = runQuery,
-  cheat = cheat
-}
+return cheat
---
+++ lua/cheat/window.lua
@@ -0,0 +1,93 @@
+local window = {}
+
+local guiStats = vim.api.nvim_list_uis()[1]
+local guiWidth = guiStats.width
+local guiHeight = guiStats.height
+
+function window.closeCurrentWindow()
+  vim.api.nvim_win_close(0, false)
+end
+
+function window.centerInWindow(str, windowWidth)
+  local shift = math.floor(windowWidth / 2) - math.floor(string.len(str) / 2)
+  return string.rep(" ", shift) .. str
+end
+
+function window.calculatePadding(guiSize, padAmount)
+  --[[
+    guiSize: The actual full size of the current UI. This is the value which will have the
+             padValue applied to it as described below.
+
+    padAmount: Represents the percentage value which will be used to size the border around
+               the floating window. For example, a padValue of 20 will will result in 20% of
+               the full window size.
+
+    Note: Neovim's `nvim_open_win` function expects an int not a float, so the value returned
+          by this function will be floored.
+  ]]
+  local paddingValue = math.floor(guiSize * (padAmount / 100))
+  return paddingValue
+end
+
+function window.setMappings(buf, mappings)
+  local modes = {"n", "i"}
+
+  for key,callback in pairs(mappings) do
+    for modeCount = 1, #modes do
+      vim.api.nvim_buf_set_keymap(
+        buf,
+        modes[modeCount],
+        key,
+        "<esc>:lua require" .. callback .. "<cr>",
+        {
+          nowait = true,
+          noremap = true,
+          silent = true
+        }
+      )
+    end
+  end
+end
+
+function window.newFloatingWindow(buf, config)
+  -- TODO: I am 100% certain this whole padding system is all jacked up and needs fixing,
+  --       but it works well enough for now.
+  --
+  -- padding - Table containing the pad values for the width and the height
+  --
+  -- config = {
+  --   padding = {
+  --     "width": 10,
+  --     "height": 10
+  --   }
+  -- }
+  local padding = config["padding"]
+
+  local widthPadding = window.calculatePadding(guiWidth, padding["width"])
+  local heightPadding = window.calculatePadding(guiHeight, padding["height"])
+
+  local windowWidth = math.floor(guiWidth - (widthPadding * 2))
+  local windowHeight = math.floor(guiHeight - (heightPadding * 2))
+
+  vim.api.nvim_buf_set_option(buf, "bufhidden", "wipe")
+
+  local newWindow = vim.api.nvim_open_win(buf, true, {
+    relative = "editor",
+    width = windowWidth,
+    height = windowHeight,
+    col = widthPadding,
+    row = heightPadding,
+    style = "minimal"
+  })
+
+  window.setMappings(buf, config["mappings"])
+
+  return newWindow
+end
+
+function window.getWidth(win)
+  local width = vim.api.nvim_win_get_width(win)
+  return width
+end
+
+return window
